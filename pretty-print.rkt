62a5bdead9e5a69f093e1a4c5eee2f3d9afc4d89
Artiom Pujleacov <119314191+artiompujleacov@users.noreply.github.com>
2024-04-20 15:14:56 +0300

    final

--- pretty-print.rkt
+++ pretty-print.rkt
@@ -3,15 +3,10 @@
 
 (provide (all-defined-out))
 
-; Numerotează elementele unei liste (asemănător cu `enumerate` din Python).
-; Returnează perechi de forma `(indice . valoare)`.
+
 (define (enumerate l) (map cons (range (length l)) l))
 
-; Formatează un ST ca un șir de caractere.
 (define (pretty-print st)
-  ; Creează prefixul pentru indentare al unui rând din desenul final. `lasts`
-  ; este o listă cu valori boolene, care pentru fiecare nivel de indentare
-  ; reține dacă elementul indentat este ultimul de pe nivelul respective.
   (define (make-indent lasts)
     (if (null? lasts) ""
         (let ([prev-last (take lasts (sub1 (length lasts)))]
@@ -20,11 +15,9 @@
            (apply string-append (map (lambda (is-last) (if is-last "    " "│   ")) prev-last))
            (if curr-last "└───" "├───")))))
 
-  ; Formatează o etichetă (un rând din desenul final).
   (define (print-label lasts str)
     (format "~a~a\n" (make-indent lasts) str))
 
-  ; Formatează recursiv un ST.
   (define (helper lasts tree)
     (let* ([label (list->string (get-branch-label tree))]
            [subs (get-branch-subtree tree)]
@@ -37,10 +30,7 @@
                                  [next-lasts (append lasts (list (= index last-index)))])
                       (helper next-lasts branch)))
                   (enumerate subs)))))
-
-  ; Ca să tratăm toate cazurile uniform, inserăm o etichetă falsă pentru
-  ; rădăcină. Fără etichetă, arborele top-level diferă de subarbori prin faptul
-  ; că subarborii sunt liste ce încep cu câte o etichetă.
+  
   (define augmented-tree (cons (string->list "*") st))
 
   (helper '() augmented-tree))
